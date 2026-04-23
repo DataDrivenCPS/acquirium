34c168eb4543a7cc65d9aa758681318647b88ab3
Gabe Fierro <gtfierro@mines.edu>
2026-04-22 23:59:31 -0600

    add Driver base class; update publish_system_metrics to use it

--- src/acquirium/__init__.py
+++ src/acquirium/__init__.py
@@ -3,3 +3,4 @@ from acquirium.Apps.base import App, Output
 from acquirium.Client.query import Query
 from acquirium.Client.data_object import DataObject
 from acquirium.internals.models import AppContext
+from acquirium.Driver import Driver
