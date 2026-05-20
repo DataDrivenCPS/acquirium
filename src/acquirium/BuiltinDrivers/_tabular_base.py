fb66fb47ec209c3e148d91cfe1f51447fc40bb82
Gabe Fierro <gtfierro@mines.edu>
2026-05-20 16:54:57 -0600

    Remove tabular base compatibility shim

--- src/acquirium/BuiltinDrivers/_tabular_base.py
+++
@@ -1,5 +0,0 @@
-"""Compatibility re-exports for the public tabular ingest base module."""
-
-from acquirium.BuiltinDrivers.tabular_base import TabularIngestBase, _safe_name
-
-__all__ = ["TabularIngestBase", "_safe_name"]
