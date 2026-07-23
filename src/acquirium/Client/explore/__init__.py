3a5b42ca76e34198d6f397eba5ac62348f3c80b5
U. Mete Saka <metesaka98@gmail.com>
2026-07-23 10:49:07 -0600

    add explore attribute registry (step 1)

---
+++ src/acquirium/Client/explore/__init__.py
@@ -0,0 +1,4 @@
+"""Explore: the redesigned Acquirium query client (core + conveniences + facets)."""
+from acquirium.Client.explore.attributes import REGISTRY, Attr, Not, normalize_value
+
+__all__ = ["Attr", "Not", "REGISTRY", "normalize_value"]
